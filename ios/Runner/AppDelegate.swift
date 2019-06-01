e35f75559a2e802293d89c86d7eddf0596b12f26
Juan Pablo Orsay <jporsay@gmail.com>
2019-06-01 11:52:11 -0300

    Bootstrap dependencies

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,12 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+//    #if DEBUG
+      var newArguments = ProcessInfo.processInfo.arguments
+      newArguments.append("-FIRDebugEnabled")
+      ProcessInfo.processInfo.setValue(newArguments, forKey: "arguments")
+      CommandLine.arguments = newArguments
+//    #endif
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
