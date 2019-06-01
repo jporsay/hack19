1c4ae8c9947c632b381b7a2851a249ba7b1206b6
Juan Pablo Orsay <jporsay@gmail.com>
2019-06-01 16:07:49 -0300

    Update stuff

--- lib/src/occasion/occasion_repository.dart
+++ lib/src/occasion/occasion_repository.dart
@@ -2,9 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hack19/src/occasion/occasion_model.dart';
 
 class OccasionRepository {
+  final _collection = Firestore.instance.collection('occasions');
+
   Stream<List<Occasion>> listConsolidated(
       {int limit = 20, DocumentSnapshot startAfter}) {
-    var query = Firestore.instance.collection('occasions');
+    var query = _collection;
     if (startAfter != null) {
       query = query.startAfterDocument(startAfter);
     }
