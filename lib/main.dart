a24637e5161c671cb80aa16bac6e58327015c6a4
Juan Pablo Orsay <jporsay@gmail.com>
2019-06-01 13:23:15 -0300

    Move home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
-import 'package:hack19/src/home_page.dart';
+import 'package:hack19/src/page/home_page.dart';
 
 void main() => runApp(MyApp());
 
---
+++ lib/src/page/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
